9a1d6c4182126e4edb3abd1e79dcfcfd59023c76
sefa baser <sefabaser84@hotmail.com>
2019-03-16 15:31:30 +0100

    version 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Comparator } from './comparator/comparator';
 export { JsonHelper } from './json-helper/json-helper';
-export { FunctionDelayer } from './function-delayer';
+export { FunctionDelayer } from './function-delayer';
 export { Guid } from './Guid';
