6dd8e72663e0916d164d2c32c92e70391bb3ba74
sefa baser <sefabaser84@hotmail.com>
2019-10-14 12:56:43 +0200

    Datetime Helper is added

---
+++ src/datetime-helper.ts
@@ -0,0 +1,13 @@
+export class DatetimeHelper {
+  static getNumberOfDaysInYear(year: number) {
+    return DatetimeHelper.isLeapYear(year) ? 366 : 355;
+  }
+
+  static isLeapYear(year: number) {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+  }
+
+  static get totalTicksInOneDay() {
+    return 24 * 60 * 60 * 1000;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { JsonHelper } from './json-helper/json-helper';
 export { FunctionDelayer } from './function-delayer';
 export { Guid } from './guid';
 export { Debouncer } from './debouncer';
+export { DatetimeHelper } from './datetime-helper';
