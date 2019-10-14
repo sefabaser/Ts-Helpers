55c0bd8d0b2633769b001ca3781be65e3c2d779f
sefa baser <sefabaser84@hotmail.com>
2019-10-14 16:50:35 +0200

    differanceInHours is implemented

--- src/datetime-helper.ts
+++ src/datetime-helper.ts
@@ -7,6 +7,10 @@ export class DatetimeHelper {
     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
   }
 
+  static differanceInHours(date1: Date, date2: Date) {
+    return Math.abs(date1.valueOf() - date2.valueOf()) / 36e5;
+  }
+
   static get totalTicksInOneDay() {
     return 24 * 60 * 60 * 1000;
   }
