8de0da01e92dbc5bf5bc6d71c0d802fb781484c5
sefa baser <sefabaser84@hotmail.com>
2019-11-06 17:43:02 +0100

    additional functions for datetime helper

--- src/datetime-helper.ts
+++ src/datetime-helper.ts
@@ -1,3 +1,5 @@
+import { Comparator } from './comparator/comparator';
+
 export class DatetimeHelper {
   static getNumberOfDaysInYear(year: number) {
     return DatetimeHelper.isLeapYear(year) ? 366 : 365;
@@ -21,11 +23,55 @@ export class DatetimeHelper {
     return Math.abs(date1.valueOf() - date2.valueOf()) / this.totalTicksInOneHour;
   }
 
+  static getDayTick(date: Date): number {
+    return Math.floor(date.valueOf() / this.totalTicksInOneDay);
+  }
+
   static get totalTicksInOneDay() {
     return 24 * 60 * 60 * 1000;
   }
 
   static get totalTicksInOneHour() {
     return 60 * 60 * 1000;
+  }
+
+  static roundDate(date: Date): Date {
+    date = new Date(date);
+    let timeStamp = date.getTime();
+    timeStamp += timeStamp % (12 * 60 * 60 * 1000);
+    timeStamp -= date.getTimezoneOffset() * 60 * 1000;
+    date = new Date(timeStamp);
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setMilliseconds(0);
+    return new Date(DatetimeHelper.dateToSimpleString(date));
+  }
+
+  static dateToSimpleString(date: Date): string {
+    return `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}-${date.getDate() < 10 ? '0' : ''}${date.getDate()}`;
+  }
+
+  static getDaysBetweenRange(from: Date, to: Date): Date[] {
+    let dateArray: Date[] = [];
+    let currentDate = from;
+    while (currentDate <= to) {
+      dateArray.push(currentDate);
+      currentDate = DatetimeHelper.addDayWithoutTimeZone(currentDate);
+    }
+    dateArray.pop();
+    return dateArray;
+  }
+
+  private static addDayWithoutTimeZone(date: Date): Date {
+    let dateCandidate = new Date(
+      `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}-${date.getDate() + 1 < 10 ? '0' : ''}${date.getDate() + 1}`
+    );
+    if (!Comparator.isDate(dateCandidate)) {
+      dateCandidate = new Date(`${date.getFullYear()}-${date.getMonth() + 2 < 10 ? '0' : ''}${date.getMonth() + 2}-01`);
+    }
+    if (!Comparator.isDate(dateCandidate)) {
+      dateCandidate = new Date(`${date.getFullYear() + 1}-01-01`);
+    }
+    return dateCandidate;
   }
 }
