79d3171fc20b42abe99648522b190592cc58127b
sefabaser <sefabaser84@hotmail.com>
2024-01-08 00:15:19 +0100

    forgotten exports are included

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { FunctionDelayer, Debouncer, Wait } from './delayer/delayer';
 export { Guid } from './random/guid';
 export { DatetimeHelper } from './datetime-helper/datetime-helper';
 export { Random } from './random/random';
-export { Vec2 } from './vector/vector';
+export { Vec2, Vec3, Vector } from './vector/vector';
+export { Radian } from './radian/radian';
