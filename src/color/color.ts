59c59a833082223f5d59e1d4266d22025374ed00
sefabaser <sefabaser84@hotmail.com>
2024-03-03 01:26:13 +0100

    color update

--- src/color/color.ts
+++ src/color/color.ts
@@ -1,19 +1,28 @@
-export interface Color {
+export interface RGBColor {
   r: number;
   g: number;
   b: number;
 }
 
 export class ColorHelper {
-  static hexColorToRGB(hex: number): Color {
+  static hexColorToRGBColor(hex: number): RGBColor {
     let hexString = hex.toString(16);
     while (hexString.length < 6) {
       hexString = '0' + hexString;
     }
 
-    let r = parseInt(hexString.slice(0, 2), 16);
-    let g = parseInt(hexString.slice(2, 4), 16);
-    let b = parseInt(hexString.slice(4, 6), 16);
+    return ColorHelper.colorNumbersToRGBColor(hexString);
+  }
+
+  static stringColorToRGBColor(color: string): RGBColor {
+    color = color.replace('#', '');
+    return ColorHelper.colorNumbersToRGBColor(color);
+  }
+
+  private static colorNumbersToRGBColor(colorNumbers: string): RGBColor {
+    let r = parseInt(colorNumbers.slice(0, 2), 16);
+    let g = parseInt(colorNumbers.slice(2, 4), 16);
+    let b = parseInt(colorNumbers.slice(4, 6), 16);
     return { r: r / 255, g: g / 255, b: b / 255 };
   }
 }
