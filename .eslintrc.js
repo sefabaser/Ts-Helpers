809db1f02b3ab6c9ab1cc4d07f2f4c8846709a40
sefabaser <sefabaser84@hotmail.com>
2024-04-26 15:14:42 +0200

    PI functions are added to number helper

--- .eslintrc.js
+++ .eslintrc.js
@@ -97,7 +97,7 @@ module.exports = {
     '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-this-alias': 'error',
     '@typescript-eslint/no-unused-expressions': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
