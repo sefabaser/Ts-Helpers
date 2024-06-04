5e48b7bbc8aef986a897b1b72cddddb4aa5baea9
sefabaser <sefabaser84@hotmail.com>
2024-06-04 10:39:43 +0200

    console helper update

--- src/console-helper/console.helper.ts
+++ src/console-helper/console.helper.ts
@@ -1,17 +1,19 @@
-const YELLOW = '\x1b[33m%s\x1b[0m';
-const CYAN = '\x1b[36m%s\x1b[0m';
-const MAGENTA = '\x1b[35m%s\x1b[0m';
-const RED = '\x1b[31m%s\x1b[0m';
-const GREEN = '\x1b[32m%s\x1b[0m';
-const BLUE = '\x1b[34m%s\x1b[0m';
-const GRAY = '\x1b[90m%s\x1b[0m';
-const WHITE = '\x1b[37m%s\x1b[0m';
+const FOR_THE_REST = '%s\x1b[0m';
+
+const YELLOW = '\x1b[33m';
+const CYAN = '\x1b[36m';
+const MAGENTA = '\x1b[35m';
+const RED = '\x1b[31m';
+const GREEN = '\x1b[32m';
+const BLUE = '\x1b[34m';
+const GRAY = '\x1b[90m';
+const WHITE = '\x1b[37m';
 
 export type ConsoleColor = 'yellow' | 'cyan' | 'magenta' | 'red' | 'green' | 'blue' | 'gray' | 'white';
 
 export class ConsoleHelper {
   static log(message: string, color: ConsoleColor): void {
-    console.log(ConsoleHelper.colorToAnsi(color), message);
+    console.log(ConsoleHelper.colorToAnsi(color) + FOR_THE_REST, message);
   }
 
   static newLine(): void {
@@ -37,5 +39,33 @@ export class ConsoleHelper {
       case 'white':
         return WHITE;
     }
+  }
+
+  static yellow(): string {
+    return YELLOW;
+  }
+
+  static cyan(): string {
+    return CYAN;
+  }
+
+  static magenta(): string {
+    return MAGENTA;
+  }
+
+  static red(): string {
+    return RED;
+  }
+
+  static green(): string {
+    return GREEN;
+  }
+
+  static blue(): string {
+    return BLUE;
+  }
+
+  static gray(): string {
+    return GRAY;
   }
 }
