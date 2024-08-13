966d49f2f58d3a1d576a6805d23fd24e76c8c59b
sefabaser <sefabaser84@hotmail.com>
2024-08-13 20:27:19 +0200

    FixedArray type is added

--- src/array-helper/array-helper.ts
+++ src/array-helper/array-helper.ts
@@ -1,3 +1,6 @@
+export type FixedArray<T, L extends number> = { length: L } & Array<T>;
+export type ReadonlyFixedArray<T, L extends number> = { length: L } & ReadonlyArray<T>;
+
 export class ArrayHelper {
   /**
    * @param length Length of the array
