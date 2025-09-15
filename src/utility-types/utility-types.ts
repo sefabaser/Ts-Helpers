2907fc1f6457d48c1980a1e96e9c993c046d7f66
sefabaser <sefabaser84@hotmail.com>
2025-09-15 19:16:19 +0200

    setup

--- src/utility-types/utility-types.ts
+++ src/utility-types/utility-types.ts
@@ -17,7 +17,37 @@ export type DeepReadonly<T> = {
         : T[P];
 };
 
-export type TypeValuePair<T extends string, M extends Record<T, any>> = {
+/**
+ * Creates type-value pair type. The type of the value will be defined by the given type or the key of the type map interface.
+ * @param M - The type map interface.
+ * @param T - The type of the key. Optional: If not provided, the key will be inferred from the type map interface.
+ * @returns The type-value pair type.
+ * @example
+ * ```ts
+ * type EventEntry = TypeValuePair<{
+ *   name: string;
+ *   age: number;
+ * }>;
+ * let a: EventEntry = {
+ *   type: 'name',
+ *   value: 'John'
+ * };
+ *
+ * let b: EventEntry = {
+ *   type: 'age',
+ *   value: 18
+ * };
+ *
+ * if (a.type === 'name') {
+ *   let value = a.value; // type of value is string
+ * }
+ *
+ * if (b.type === 'age') {
+ *   let value = b.value; // type of value is number
+ * }
+ * ```
+ */
+export type TypeValuePair<M extends Record<T, any>, T extends string = keyof M & string> = {
   [K in T]: {
     readonly type: K;
     readonly value: M[K];
