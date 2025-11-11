81faed19e8f1d43e8013bc32d9919b8364d8124e
sefabaser <sefabaser84@hotmail.com>
2025-11-11 13:43:36 +0100

    setup changes

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig(({ mode }) => ({
+  base: './',
+  test: {
+    setupFiles: ['./vitest.setup.ts'],
+    fakeTimers: {
+      toFake: ['setTimeout', 'setInterval', 'queueMicrotask'],
+    },
+    env: {
+      QUICK: mode === 'quick' ? '1' : undefined
+    }
+  },
+}));
